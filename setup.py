c86c78140eecf756be5cfd0e64d57dea1d1b7eeb
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-11 22:35:42 +0200

    Added setup script

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "accscout",
+    version = "1.0",
+    author = "Richard Antal Nagy",
+    license = "MIT",
+    keywords = [ "accscout", "account", "security", "hacking", "OSINT", "crawler" ],
+    url = "https://gitlab.com/richardnagy/security/accscout",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Topic :: Security",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ],
+    install_requires=[
+        'pyyaml',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'accscout = accscout.__main__:main'
+        ]
+    },
+)
