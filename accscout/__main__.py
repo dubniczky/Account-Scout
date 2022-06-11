1de6147fde318173b92f7ca00e4977181ad2739f
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-12 00:50:33 +0200

    Verified cli input length in main

--- accscout/__main__.py
+++ accscout/__main__.py
@@ -4,6 +4,10 @@ import sys
 from accscout import scout
 
 def main():
+    if len(sys.argv < 2):
+        print('accscout [USERNAME]')
+        exit(1)
+    
     username = sys.argv[1]
     scout(username)
 
