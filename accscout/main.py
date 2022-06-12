e12b438d22f321eae5e8e9789432f75f9a4728c5
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-12 13:19:27 +0200

    Added support for mulriple users at once #1

--- accscout/main.py
+++ accscout/main.py
@@ -6,6 +6,7 @@ from requests import Session
 from yaml import safe_load, safe_dump
 from threading import Thread
 
+
 # Load pages config
 pages: None|dict = None
 try:
@@ -14,6 +15,7 @@ try:
 except:
     print('Error, coult not read page configuration.')
     exit(1)
+
 
 # Load header config
 headers: None|dict = None
@@ -24,8 +26,10 @@ except:
     print('Error, coult not read header configuration.')
     exit(1)
 
+
 def printc(color: tuple[int,int,int], text: str) -> None:
     print( f"\033[38;2;{color[0]};{color[1]};{color[2]}m{text}\033[0m" )
+
 
 def scout_page(session, username, page_name, url):
 
@@ -44,11 +48,12 @@ def scout_page(session, username, page_name, url):
     # Print result
     printc( color,  f'[ {res.status_code} ] ({elapsed}ms) {page_name}: {url}' )
 
-def scout(username):
+
+def scout(usernames):
     session = Session()
 
     # Print general info
-    print('Scouting user:', username)
+    print('Scouting user(s):', usernames)
     print('Page count:', len(pages))
     print('Headers:')
     print('========================================')
@@ -61,31 +66,33 @@ def scout(username):
 
     # Start threads for each request
     threads = []
-    start_time = process_time()
-    for page in pages:
-        page_name: str = page['name']
-        url: str = page['url'].replace('{!!}', username)
-        t = Thread(target=scout_page, args=(session, username, page_name, url))
-        threads.append(t)
-        t.start()
+    for user in usernames:
+        for page in pages:
+            page_name: str = page['name']
+            url: str = page['url'].replace('{!!}', user)
+            t = Thread(target=scout_page, args=(session, user, page_name, url))
+            threads.append(t)
+            t.start()
 
     # Wait for finish
     for t in threads:
         t.join()
 
+
+def main():
+    start_time = process_time()
+    if len(sys.argv) < 2:
+        print('Usage: accscout [USERNAME]... ')
+        print('Scout users on popular websites.')
+        exit(1)
+    
+    usernames = sys.argv[1:]
+    scout(usernames)
+
     end_time = process_time()
     delta_time = end_time - start_time
     print('========================================')
-    print(f'Completed {len(pages)} requests in {delta_time}s')
-
-
-def main():
-    if len(sys.argv) < 2:
-        print('accscout [USERNAME]')
-        exit(1)
-    
-    username = sys.argv[1]
-    scout(username)
+    print(f'Completed {len(pages) * len(usernames)} requests in {delta_time}s')
 
 
 if __name__ == '__main__':
